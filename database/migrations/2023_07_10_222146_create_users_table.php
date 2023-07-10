9b7de125535ce707e578f9e3a266bb71afce453b
barsu5489 <ebarsulai@gmail.com>
2023-07-11 01:52:37 +0300

    finished product

--- database/migrations/2023_07_10_222146_create_users_table.php
+++ database/migrations/2023_07_10_222146_create_users_table.php
@@ -4,24 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->text('specifications');
+            $table->string('brand')->nullable();
+            $table->string('model')->nullable();
+            $table->integer('ram')->nullable();
+            $table->integer('storage')->nullable();
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
-};
+}
