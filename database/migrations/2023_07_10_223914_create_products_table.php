f408189cead1e1c8d18b580e02421eb79b29eb9e
barsu5489 <ebarsulai@gmail.com>
2023-07-11 01:57:11 +0300

    product, user fix

--- database/migrations/2023_07_10_223914_create_products_table.php
+++ database/migrations/2023_07_10_223914_create_products_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->text('specifications');
+            $table->string('brand')->nullable();
+            $table->string('model')->nullable();
+            $table->integer('ram')->nullable();
+            $table->integer('storage')->nullable();
+
             $table->timestamps();
         });
     }
