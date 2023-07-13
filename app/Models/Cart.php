b09b2f33fdf5846d72caf21530701c8598f3e2e8
barsu5489 <ebarsulai@gmail.com>
2023-07-13 21:30:09 +0300

    associations

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the user associated with the cart.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the products associated with the cart.
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
