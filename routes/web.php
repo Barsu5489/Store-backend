5c04797f0385fc53685052f3f3ce43ac7235ad55
barsu5489 <ebarsulai@gmail.com>
2023-07-13 22:19:29 +0300

    contollers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
